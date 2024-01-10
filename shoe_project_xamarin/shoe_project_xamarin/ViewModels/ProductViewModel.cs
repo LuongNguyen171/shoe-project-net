467cee6d4584b07a5933c21483432049d88b5a49
LuongNguyen171 <luongnguyen170102@gmail.com>
2024-01-10 10:06:39 +0700

    .

---
+++ shoe_project_xamarin/shoe_project_xamarin/ViewModels/ProductViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Android.Net;
+using Newtonsoft.Json;
+using shoe_project_server.Models.HostConfig;
+using shoe_project_xamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoe_project_xamarin.ViewModels
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        readonly ApiSettings apiSettings = new ApiSettings();
+        private ObservableCollection<Product> _newProductList;
+        private ObservableCollection<Product> _trendsList;
+        public ObservableCollection<Product> NewProductList
+        {
+            get { return _newProductList; }
+            set
+            {
+                if (_newProductList != value)
+                {
+                    _newProductList = value;
+                    OnPropertyChanged(nameof(NewProductList));
+                }
+            }
+        }
+        public ObservableCollection<Product> TrendsList
+        {
+            get { return _trendsList; }
+            set
+            {
+                if (_trendsList != value)
+                {
+                    _trendsList = value;
+                    OnPropertyChanged(nameof(TrendsList));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public async Task LoadDataAsync()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string apiUrl = apiSettings.BuildApiClientHost("/Product/getAllProducts");
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    string content = await response.Content.ReadAsStringAsync();
+
+                    List<Product> products = JsonConvert.DeserializeObject<List<Product>>(content);
+
+                    int skipCount = Math.Max(0, products.Count - 5);
+                    List<Product> selectedProducts = products.Skip(skipCount).Take(5).ToList();
+                    NewProductList = new ObservableCollection<Product>(selectedProducts);
+
+                    TrendsList = new ObservableCollection<Product>(products.Take(5));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
--- shoe_project_xamarin/shoe_project_xamarin/Views/Pages/Home.xaml.cs
+++ shoe_project_xamarin/shoe_project_xamarin/Views/Pages/Home.xaml.cs
@@ -1,22 +1,87 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using shoe_project_server.Models.HostConfig;
+using shoe_project_xamarin.Models;
+using shoe_project_xamarin.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Timers;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static shoe_project_xamarin.Views.Pages.Home;
 
 namespace shoe_project_xamarin.Views.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Home : ContentPage
     {
+        private Timer timer;
+        public List<Banner> Banners { get => GetBanners(); }
+
+        private ProductViewModel _viewModel;
+
         public Home()
         {
             InitializeComponent();
+            _viewModel = new ProductViewModel();
+            cvBanners.ItemsSource = Banners;
+            BindingContext = _viewModel;
+            LoadData();
         }
 
-      
+        private async void LoadData()
+        {
+            await _viewModel.LoadDataAsync();
+        }
+
+        private List<Banner> GetBanners()
+        {
+            var bannerList = new List<Banner>();
+            bannerList.Add(new Banner { Heading = "ADIDAS COLLECTION", Message = "40% Discount", Caption = "BEST DISCOUNT THIS SEASON", Image = "banner1.jpg" });
+            bannerList.Add(new Banner { Heading = "NIKE COLLECTION", Message = "UP TO 50% OFF", Caption = "GET 50% OFF ON EVERY ITEM", Image = "banner2.jpg" });
+            bannerList.Add(new Banner { Heading = "SUMMER COLLECTION", Message = "20% Discount", Caption = "UNIQUE COMBINATIONS OF ITEMS", Image = "banner3.jpg" });
+            return bannerList;
+        }
+
+        protected override void OnAppearing()
+        {
+            timer = new Timer(TimeSpan.FromSeconds(3).TotalMilliseconds) { AutoReset = true, Enabled = true };
+            timer.Elapsed += Timer_Elapsed;
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            timer?.Dispose();
+            base.OnDisappearing();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+
+                if (cvBanners.Position == 2)
+                {
+                    cvBanners.Position = 0;
+                    return;
+                }
+
+                cvBanners.Position += 1;
+            });
+        }
+
+        public class Banner
+        {
+            public string Heading { get; set; }
+            public string Message { get; set; }
+            public string Caption { get; set; }
+            public string Image { get; set; }
+        }
     }
 }
