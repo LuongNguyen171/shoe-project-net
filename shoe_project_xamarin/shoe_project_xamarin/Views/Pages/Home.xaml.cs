f91b117a3d452d9f0ff6a134e134c4443eaad8bd
LuongNguyen171 <luongnguyen170102@gmail.com>
2024-01-10 10:31:20 +0700

    fix Carousel

--- shoe_project_xamarin/shoe_project_xamarin/Views/Pages/Home.xaml.cs
+++ shoe_project_xamarin/shoe_project_xamarin/Views/Pages/Home.xaml.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 using System.Timers;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static Android.Provider.MediaStore;
 using static shoe_project_xamarin.Views.Pages.Home;
 
 namespace shoe_project_xamarin.Views.Pages
@@ -29,9 +30,25 @@ namespace shoe_project_xamarin.Views.Pages
         {
             InitializeComponent();
             _viewModel = new ProductViewModel();
-            cvBanners.ItemsSource = Banners;
+          /*  cvBanners.ItemsSource = Banners;*/
             BindingContext = _viewModel;
             LoadData();
+
+            List<Images> images = new List<Images>()
+            {
+
+                new Images(){Title="Image 1",Url="https://tokyolife.vn/wp/wp-content/uploads/2022/08/z3605752590998_a9af1d076b9f454e302e7080b42ceb3d-1-1024x642.jpg"},
+                new Images(){Title="Image 2",Url="http://file.hstatic.net/1000326314/article/1-_b40d8dad86db43afb36cc889dbb7f723.jpg"},
+                new Images(){Title="Image 3",Url="https://cdn.tgdd.vn//News/1468021//giamgia-quan-ao-giay-dep-845x414.jpg"},
+                new Images(){Title="Image 3",Url="udao.jpg"},
+            };
+
+            Device.StartTimer(TimeSpan.FromSeconds(2), (Func<bool>)(() =>
+            {
+                Carousel.Position = (Carousel.Position + 1) % images.Count;
+                return true;
+            }));
+            Carousel.ItemsSource = images;
         }
 
         private async void LoadData()
@@ -47,7 +64,7 @@ namespace shoe_project_xamarin.Views.Pages
             bannerList.Add(new Banner { Heading = "SUMMER COLLECTION", Message = "20% Discount", Caption = "UNIQUE COMBINATIONS OF ITEMS", Image = "banner3.jpg" });
             return bannerList;
         }
-
+/*
         protected override void OnAppearing()
         {
             timer = new Timer(TimeSpan.FromSeconds(3).TotalMilliseconds) { AutoReset = true, Enabled = true };
@@ -75,7 +92,7 @@ namespace shoe_project_xamarin.Views.Pages
                 cvBanners.Position += 1;
             });
         }
-
+*/
         public class Banner
         {
             public string Heading { get; set; }
@@ -83,7 +100,11 @@ namespace shoe_project_xamarin.Views.Pages
             public string Caption { get; set; }
             public string Image { get; set; }
         }
+        public class Images
+        {
+            public string Title { get; set; }
+            public string Url { get; set; }
+        }
 
-      
     }
 }
